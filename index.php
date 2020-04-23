dad0694b0b773cf73d0cb22302549e3893c184a4
Michael Gulchuk <mgulchuk@mail.greenriver.edu>
2020-04-22 22:42:42 -0400

    added require file functions.php

--- index.php
+++ index.php
@@ -4,15 +4,11 @@
  * 4/17/20
  * https://mgulchuk.greenriverdev.com/IT328/pp2/index.php
  */
-
+require("functions.php");
 echo "PHP Array Practice";
 echo "<br>";
 echo "<br>";
 $numbers = array(7, 9, 8, 9, 8, 8, 6);
-function printArr($array){
-    foreach($array as $value){
-        echo "$value<br>";
-    }
-}
+
 
 printArr($numbers);
