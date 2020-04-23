98518c05cd6c168144002ed2b122499c0ce261b4
Michael Gulchuk <mgulchuk@mail.greenriver.edu>
2020-04-22 22:38:04 -0400

    Added printArr() function

--- index.php
+++ index.php
@@ -5,4 +5,14 @@
  * https://mgulchuk.greenriverdev.com/IT328/pp2/index.php
  */
 
-echo "PPH Array Practice";
+echo "PHP Array Practice";
+echo "<br>";
+echo "<br>";
+$numbers = array(7, 9, 8, 9, 8, 8, 6);
+function printArr($array){
+    foreach($array as $value){
+        echo "$value<br>";
+    }
+}
+
+printArr($numbers);
